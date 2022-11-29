4e4547f9ea556a0700ef34ac207258d70950984e
pvgomes <paulo.gomes@nubank.com.br>
2022-11-28 21:40:59 -0600

    starting search component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import CardList from "./CardList";
+import SearchBox from "./SearchBox";
 import {robots} from './robots'
 
 const App = () => {
     return (
-        <div>
+        <div className="tc">
             <h1>Robot friends</h1>
+            <SearchBox />
             <CardList robots={robots} />
         </div>
         );
