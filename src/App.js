8aecfa8bbbe3ab05301b65f0bf94525da1faa473
pvgomes <paulo.gomes@nubank.com.br>
2022-11-29 08:39:59 -0600

    strt to use state approach

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import {robots} from './robots'
 
-const App = () => {
-    return (
+class App extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange(event) {
+        console.log(event);
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+    }
+
+    render () {
+        return (
         <div className="tc">
             <h1>Robot friends</h1>
-            <SearchBox />
-            <CardList robots={robots} />
+            <SearchBox searchChange={this.onSearchChange} />
+            <CardList robots={this.state.robots} />
         </div>
-        );
+        )
+        
+    } 
 }
 
 export default App;
